10417f3f7b07c8e6a01bd4249f3ba4e96ef09c4c
unknown <amanda.wisniowski@gmail.com>
2018-04-07 18:06:46 -0500

    fixed typing bug, added step history, game fully working

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -19,7 +19,7 @@ function Square(props) {
       return ( 
       <Square 
         value={this.props.squares[i]}
-        onClick={() => this.props.on(i)} 
+        onClick={() => this.props.onClick(i)} 
       />
     );
   }
@@ -56,12 +56,13 @@ function Square(props) {
           history: [{
             squares: Array(9).fill(null),
           }],
+          stepNumber:0,
           xIsNext: true,
         };
       }
 
       handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
       
@@ -73,13 +74,21 @@ function Square(props) {
             history: history.concat([{
                 squares: squares,
               }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
+    jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
+        });
+      }
+
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
     
         const moves = history.map((step, move) => {
@@ -87,7 +96,7 @@ function Square(props) {
               'Go to move #' + move :
               'Go to game start';
             return (
-              <li>
+              <li key={move}>
                 <button onClick={() => this.jumpTo(move)}>{desc}</button>
               </li>
             );
