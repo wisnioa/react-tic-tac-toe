9f1c5bd91825775da568ed880a75ae518b0e9af4
unknown <amanda.wisniowski@gmail.com>
2018-04-07 15:48:21 -0500

    new square component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,18 +5,12 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value:null,
-        };
-    }
-
+ 
     render() {
       return (
         <button className="square" onClick={() =>
-            this.setState({value: 'X'})}>
-          {this.state.value}
+            this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
