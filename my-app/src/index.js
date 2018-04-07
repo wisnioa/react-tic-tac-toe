01e57fa7acfeed8fd95971ddef4e1e2dd2f632ce
unknown <amanda.wisniowski@gmail.com>
2018-04-07 17:12:36 -0500

    adding handleClick function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,6 +23,13 @@ class Square extends React.Component {
             squares: Array(9).fill(null),
         };
     }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+    
     renderSquare(i) {
       return ( 
       <Square 
